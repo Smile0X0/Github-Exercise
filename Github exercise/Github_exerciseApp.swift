62fc6c2309ffd1d2873c0ceb6d129dcfb98eb678
Kamil <vamos00c@gmail.com>
2022-10-18 12:41:37 +0200

    Initial Commit

---
+++ Github exercise/Github_exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_exerciseApp.swift
+//  Github exercise
+//
+//  Created by Kamil Janczak on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Github_exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
